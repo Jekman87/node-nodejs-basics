1b5aa4dde4cbedaad419e0d4bc7d6b4005d9ab8c
Jekman87 <jeka_zzz@mail.ru>
2024-01-30 01:31:15 +0300

    refactor: add empty lines

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,11 +1,11 @@
-import { readdir } from "fs/promises";
+import { readdir } from 'fs/promises';
 import { createPathFromUrl } from '../utils.js';
 
 const list = async () => {
     const dirPath = createPathFromUrl(import.meta.url, 'files');
 
     try {
-        const files = await readdir(dirPath)
+        const files = await readdir(dirPath);
         console.log(files);
     } catch (err) {
         throw new Error('FS operation failed');
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,4 @@
-import { readFile } from "fs/promises";
+import { readFile } from 'fs/promises';
 import { createPathFromUrl } from '../utils.js';
 
 const read = async () => {
