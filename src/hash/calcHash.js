2a0a3eace8a0c9c5a54c373269dc9aa73f1d1df7
Jekman87 <jeka_zzz@mail.ru>
2024-01-29 23:50:24 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { createPathFromUrl } from '../utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileForHash = createPathFromUrl(
+        import.meta.url,
+        'files/fileToCalculateHashFor.txt'
+    );
+    const hash = createHash('sha256');
+
+    const stream = createReadStream(fileForHash);
+
+    stream.on('readable', () => {
+        const data = stream.read();
+
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(hash.digest('hex'));
+        }
+    });
 };
 
 await calculateHash();
