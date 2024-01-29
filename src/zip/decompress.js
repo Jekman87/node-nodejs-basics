dc088fb3b8db5117cc2f551fdd55c46b59f2794b
Jekman87 <jeka_zzz@mail.ru>
2024-01-30 00:35:53 +0300

    feat: implement zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import zlib from 'zlib';
+import { createPathFromUrl } from '../utils.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const compressedFile = createPathFromUrl(
+        import.meta.url,
+        'files/archive.gz'
+    );
+    const decompressedFile = createPathFromUrl(
+        import.meta.url,
+        'files/fileToCompress.txt'
+    );
+
+    pipeline(
+        createReadStream(compressedFile),
+        zlib.createGunzip(),
+        createWriteStream(decompressedFile),
+        (err) => {
+            if (err) {
+                console.error('Decompression failed!', err);
+            } else {
+                console.log('Decompression succeeded!');
+            }
+        }
+    );
 };
 
 await decompress();
