e88a6cfd0dc918f1cdc89c9b5e481ef0eddc061b
Jekman87 <jeka_zzz@mail.ru>
2024-01-30 00:23:38 +0300

    feat: implement streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { createPathFromUrl } from '../utils.js';
+
 const write = async () => {
-    // Write your code here 
+    const fileToWrite = createPathFromUrl(
+        import.meta.url,
+        'files/fileToWrite.txt'
+    );
+    const writeStream = createWriteStream(fileToWrite);
+
+    process.stdin.on('data', (chunk) => {
+        writeStream.write(chunk.toString());
+    });
 };
 
 await write();
